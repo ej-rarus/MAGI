8504c016295c5a32f476280557c7dc77395f2ff3
Eunjae Tony Lee <lpl2001@naver.com>
2025-06-13 17:51:45 +0900

    v0.0.5: 새로운 SVG 파일(Balthasar, Melchior, Casper) 추가 및 Connect 컴포넌트를 생성하여 SVG 간의 연결선을 그리는 기능을 구현했습니다. Vite 설정에 svgr 플러그인을 추가하고, CSS 스타일을 수정하여 레이아웃을 개선했습니다. package.json 및 package-lock.json에 vite-plugin-svgr 의존성을 추가했습니다.

---
+++ src/components/Connect.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface ConnectProps {
+    x1: number | string;
+    y1: number | string;
+    x2: number | string;
+    y2: number | string;
+}
+
+const Connect: React.FC<ConnectProps> = ({ x1, y1, x2, y2 }) => {
+    return (
+        <line
+            x1={x1}
+            y1={y1}
+            x2={x2}
+            y2={y2}
+            stroke="var(--theme-color)"
+            strokeWidth="25"
+        />
+    );
+};
+
+export default Connect;
--- src/pages/Magi.tsx
+++ src/pages/Magi.tsx
@@ -1,10 +1,27 @@
 import '../styles/magi.css'
+import Balthasar from '../assets/BALTHASAR.svg?react';
+import Melchior from '../assets/MELCHIOR.svg?react';
+import Casper from '../assets/CASPER.svg?react';
+import Connect from '../components/Connect';
 
 function Magi() {
   return (
     <div id="app-container" className="container">
         <div id="magi-container" className="container">
+            <svg width="100%" height="100%" style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none' }}>
+                {/* Balthasar(중앙) -> Melchior(오른쪽) */}
+                <Connect x1="50%" y1="30%" x2="80%" y2="85%" />
+                
+                {/* Melchior(오른쪽) -> Casper(왼쪽) */}
+                <Connect x1="80%" y1="85%" x2="20%" y2="85%" />
+                
+                {/* Casper(왼쪽) -> Balthasar(중앙) */}
+                <Connect x1="20%" y1="85%" x2="50%" y2="30%" />
+            </svg>
 
+            <Balthasar className="balthasar-svg" />
+            <Melchior className="melchior-svg" />
+            <Casper className="casper-svg" />
         </div>
         <div id="input-container" className="container">
             <div id="input-title">QUESTION:</div>
